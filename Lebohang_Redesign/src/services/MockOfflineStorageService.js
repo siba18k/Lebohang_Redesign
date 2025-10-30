cb2e29818fa7d27f913dccab9ca3a631a4f4e45e
Sibahle Dube <112499606+siba18k@users.noreply.github.com>
2025-10-30 17:14:54 +0200

    Delete Lebohang_Redesign directory

--- Lebohang_Redesign/src/services/MockOfflineStorageService.js
+++
@@ -1,8 +0,0 @@
-// Mock offline storage service
-export const OfflineStorageService = {
-  getUser: () => Promise.resolve(null),
-  saveUser: (userData) => Promise.resolve(),
-  getScans: () => Promise.resolve([]),
-  saveScan: (scanData) => Promise.resolve(),
-  syncData: () => Promise.resolve()
-};
